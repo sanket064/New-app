dc2475d9e5320e53f1206aff025ec3914bf26ffb
sanket064 <sanket064@gmail.com>
2019-08-12 09:55:23 -0700

    Ui Design changes

--- www/js/index.js
+++ www/js/index.js
@@ -357,3 +357,16 @@ var app = {
         });
     }
 };
+$( document ).bind( 'mobileinit', function(){
+    $.mobile.loader.prototype.options.text = "loading";
+    $.mobile.loader.prototype.options.textVisible = false;
+    $.mobile.loader.prototype.options.theme = "a";
+    $.mobile.loader.prototype.options.html = "";
+  });
+  $.mobile.loading( 'show', {
+	text: 'foo',
+	textVisible: true,
+	theme: 'z',
+	html: ""
+});
+   
